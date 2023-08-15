f899702aa9be51b1f200d30fc3b21e387e0fe0cb
Mboyafm <fidelmartin02@gmail.com>
2023-08-15 15:57:54 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alaphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char j;
+	for (j = 'a'; j <= 'z'; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
