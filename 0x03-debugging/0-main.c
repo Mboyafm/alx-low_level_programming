841d27bf7654064a978796c64c2523f4a1369381
Mboyafm <fidelmartin02@gmail.com>
2023-08-16 09:22:42 -0700

    main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Tests whether number is positive or negative
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
