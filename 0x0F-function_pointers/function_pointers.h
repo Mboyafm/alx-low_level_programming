4492e57596912f790fa2403481a3f35a3a5e6811
Mboyafm <fidelmartin02@gmail.com>
2023-09-14 09:25:13 -0700

    3-op_functions.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -16,5 +16,16 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 
 int int_index(int *array, int size, int (*cmp)(int));
 
+int op_add(int a, int b);
+
+int op_function(int param1, double param2);
+int (*f)(int a, int b);
+int op_sub(int a, int b);
+
+int op_mul(int a, int b);
+
+int op_div(int a, int b);
+
+int op_mod(int a, int b);
 
 #endif
